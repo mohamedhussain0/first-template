effec96f4bdb89cf38cf071163aa4a9a6f09d86c
Mohamed Hussain <enghamo2005@gmail.com>
2025-01-09 15:42:50 +0200

    update some changes

--- js/position.js
+++ js/position.js
@@ -2,9 +2,6 @@ let header = document.getElementsByTagName("header")[0];
 let UPbtn = document.getElementById("up-btn");
 
 window.onscroll = () => {
-  if (scrollY <= 900) header.style.position = "fixed";
-  else header.style.position = "absolute";
-
   if (scrollY >= 900) UPbtn.style.display = "block";
   else UPbtn.style.display = "none";
 };
